42f898f9d0945dd901aa4ef33c3c7a27ced3ad5a
kiran <kiranrakesh@gmail.com>
2021-05-14 15:51:31 -0500

    Add category service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { MenuComponent } from './menu/menu.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/category/category.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+const localUrl = 'http://localhost:9092/api/categories';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor( private http: HttpClient) { }
+
+  getCategories(): any {
+    return this.http
+      .get(`${localUrl}`);
+  }
+}
