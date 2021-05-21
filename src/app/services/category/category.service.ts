ab8501d737a57b0525de7aba863f4af1ccfccdd6
kiran <kiranrakesh@gmail.com>
2021-05-21 10:51:05 -0500

    Change made

--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
   // const url = 'http://localhost:9092';
- const url = 'https:/lit-falls-22870.herokuapp.com/';
+ const url = 'https://lit-falls-22870.herokuapp.com/';
 
 @Injectable({
   providedIn: 'root'
